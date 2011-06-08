4dc440ab8ca8493b1ffbb8f5db0ab9aa995143f2
conates <o.p.t.e.r.o.n.86@gmail.com>
2011-06-08 16:52:28 -0400

    borrar cache y templeates

--- cache/backend/prod/modules/autoAdministrarProductos/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="3">
-  <?php echo __('%%id%% - %%nombre%% - %%precio_venta%%', array('%%id%%' => link_to($producto->getId(), 'producto_edit', $producto), '%%nombre%%' => $producto->getNombre(), '%%precio_venta%%' => $producto->getPrecioVenta()), 'messages') ?>
-</td>
--- cache/backend/prod/modules/autoAdministrarProductos/templates/_list_th_stacked.php
+++
@@ -1,1 +0,0 @@
-<?php include_partial('AdministrarProductos/list_th_tabular', array('sort' => $sort)) ?>
